3f86c1d58ff9445ae41947c434f65808fe5424d8
Achille Morenville <achille.morenville@uclouvain.be>
2023-06-20 16:53:11 +0200

    Update for local grid

--- aut_spot/aut_spot/spot_data.py
+++ aut_spot/aut_spot/spot_data.py
@@ -10,8 +10,62 @@ from bosdyn.client.robot_state import RobotStateClient
 from bosdyn.client.world_object import WorldObjectClient
 from bosdyn.api import world_object_pb2
 from bosdyn.client.frame_helpers import get_a_tform_b, VISION_FRAME_NAME, ODOM_FRAME_NAME, BODY_FRAME_NAME, GRAV_ALIGNED_BODY_FRAME_NAME
+from bosdyn.api import local_grid_pb2
+from bosdyn.client.local_grid import LocalGridClient
 
 from aut_msgs.msg import Fiducial
+from aut_msgs.msg import LocalGrid
+
+def get_numpy_data_type(local_grid_proto):
+	"""Convert the cell format of the local grid proto to a numpy data type."""
+	if local_grid_proto.cell_format == local_grid_pb2.LocalGrid.CELL_FORMAT_UINT16:
+		return np.uint16
+	elif local_grid_proto.cell_format == local_grid_pb2.LocalGrid.CELL_FORMAT_INT16:
+		return np.int16
+	elif local_grid_proto.cell_format == local_grid_pb2.LocalGrid.CELL_FORMAT_UINT8:
+		return np.uint8
+	elif local_grid_proto.cell_format == local_grid_pb2.LocalGrid.CELL_FORMAT_INT8:
+		return np.int8
+	elif local_grid_proto.cell_format == local_grid_pb2.LocalGrid.CELL_FORMAT_FLOAT64:
+		return np.float64
+	elif local_grid_proto.cell_format == local_grid_pb2.LocalGrid.CELL_FORMAT_FLOAT32:
+		return np.float32
+	else:
+		return None
+
+def expand_data_by_rle_count(local_grid_proto, data_type=np.int16):
+	"""Expand local grid data to full bytes data using the RLE count."""
+	cells_pz = np.frombuffer(local_grid_proto.local_grid.data, dtype=data_type)
+	cells_pz_full = []
+	# For each value of rle_counts, we expand the cell data at the matching index
+	# to have that many repeated, consecutive values.
+	for i in range(0, len(local_grid_proto.local_grid.rle_counts)):
+		for j in range(0, local_grid_proto.local_grid.rle_counts[i]):
+			cells_pz_full.append(cells_pz[i])
+	return np.array(cells_pz_full)
+
+def unpack_grid(local_grid_proto):
+	"""Unpack the local grid proto."""
+	# Determine the data type for the bytes data.
+	data_type = get_numpy_data_type(local_grid_proto.local_grid)
+	if data_type is None:
+		print("Cannot determine the dataformat for the local grid.")
+		return None
+	# Decode the local grid.
+	if local_grid_proto.local_grid.encoding == local_grid_pb2.LocalGrid.ENCODING_RAW:
+		full_grid = np.frombuffer(local_grid_proto.local_grid.data, dtype=data_type)
+	elif local_grid_proto.local_grid.encoding == local_grid_pb2.LocalGrid.ENCODING_RLE:
+		full_grid = expand_data_by_rle_count(local_grid_proto, data_type=data_type)
+	else:
+		# Return nothing if there is no encoding type set.
+		return None
+	# Apply the offset and scaling to the local grid.
+	if local_grid_proto.local_grid.cell_value_scale == 0:
+		return full_grid
+	full_grid_float = full_grid.astype(np.float64)
+	full_grid_float *= local_grid_proto.local_grid.cell_value_scale
+	full_grid_float += local_grid_proto.local_grid.cell_value_offset
+	return full_grid_float
 
 def create_TransformStamped(bosdyn_transform, frame_id, child_frame_id, stamp):
 	trans = TransformStamped()
@@ -70,6 +124,7 @@ class SpotData(Node):
 		# self.vo_publisher = self.create_publisher(Odometry, "aut_spot/odometry/v_odom", 10)
 
 		self.fiducials_publisher = self.create_publisher(Fiducial, "aut_spot/fiducial", 10)
+		self.local_grid_publisher = self.create_publisher(LocalGrid, "aut_spot/local_grid", 10)
 
 		# TF2 Broadcaster
 		self.tf_broadcaster = TransformBroadcaster(self)
@@ -94,6 +149,12 @@ class SpotData(Node):
 
 		timer_period_fiducials = 1.0 / self.spot_fiducials_frequency
 		self.timer_fiducials = self.create_timer(timer_period_fiducials, self.fiducials_call_back)
+
+		# Launch LocalGrids
+		self.local_grid_client = self.robot.ensure_client(LocalGridClient.default_service_name)
+
+		timer_period_local_grids = 1.0 / self.spot_local_grids_frequency
+		self.timer_local_grids = self.create_timer(timer_period_local_grids, self.local_grids_call_back)
 
 	def state_call_back(self):
 		robot_state = self.robot_state_client.get_robot_state()
@@ -206,11 +267,60 @@ class SpotData(Node):
 
 			self.fiducials_publisher.publish(fiducial)
 
-			fid = create_TransformStamped(body_tform_fiducial, "base_link", f"fiducial_{tag_id}", stamp)
+			fid = create_TransformStamped(body_tform_fiducial, "base_link", f"fiducial_{tag_id}", stamp_real)
 			
 			self.tf_broadcaster.sendTransform(fid)
 
 			self.get_logger().info('Published fiducial')
+
+	def local_grids_call_back(self):
+		local_grid_protos = local_grid_client.get_local_grids(["obstacle_distance"])
+
+		local_grid_transforms = local_grid_protos[0].local_grid.transforms_snapshot
+
+		body_tform_grid = get_a_tform_b(
+			local_grid_transforms,
+			BODY_FRAME_NAME,
+			local_grid_protos[0].local_grid.frame_name_local_grid_data
+		)
+
+		cells_obstacle_dist = unpack_grid(local_grid_protos[0]).astype(np.float32)
+
+		timestamp = local_grid_protos[0].acquisition_time
+
+		seconds_real = timestamp.seconds - self.robot.time_sync.get_robot_clock_skew().seconds
+		nanos_real = timestamp.nanos - self.robot.time_sync.get_robot_clock_skew().nanos
+
+		if nanos_real < 0:
+			nanos_real = nanos_real + 1_000_000_000
+			seconds_real = seconds_real - 1
+
+		stamp_real = Time()
+		stamp_real.sec = seconds_real
+		stamp_real.nanosec = nanos_real
+
+		local_grid = LocalGrid()
+
+		local_grid.header.stamp = stamp_real
+		local_grid.header.frame_id = "base_link"
+		local_grid.local_grid = cells_obstacle_dist
+
+		local_grid.pose.translation.x = body_tform_grid.x
+		local_grid.pose.translation.y = body_tform_grid.y
+		local_grid.pose.translation.z = body_tform_grid.z
+
+		local_grid.pose.rotation.x = body_tform_grid.rot.x
+		local_grid.pose.rotation.y = body_tform_grid.rot.y
+		local_grid.pose.rotation.z = body_tform_grid.rot.z
+		local_grid.pose.rotation.w = body_tform_grid.rot.w
+
+		self.local_grid_publisher.publish(local_grid)
+
+		lg = create_TransformStamped(body_tform_grid, "base_link", "local_grid", stamp_real)
+			
+		self.tf_broadcaster.sendTransform(lg)
+
+		self.get_logger().info('Published local grid')
 
 
 def main(args=None):
